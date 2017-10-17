27cfda1003bce798c620c44659cf7bd8e70ed7f2
niom <marko.pukari@nitor.fi>
2017-10-17 17:41:04 +0300

    Add react-navigation and simple functionality - Tests are failing

--- App.js
+++ App.js
@@ -4,54 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  StackNavigator,
+} from 'react-navigation';
+import MainScreen from "./MainScreen"
+import ProfileScreen from "./ProfileScreen"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const BasicApp = StackNavigator({
+  Main: {screen: MainScreen},
+  Profile: {screen: ProfileScreen},
 });
